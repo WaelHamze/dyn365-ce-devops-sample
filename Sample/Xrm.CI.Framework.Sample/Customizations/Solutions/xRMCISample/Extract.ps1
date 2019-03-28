1cc59dbf59c96aaefc2bb180c66c502eb04cf65c
Wael Hamze <contact@waelhamze.com>
2019-03-28 21:15:10 +0000

    Updated to latest SDK and CI Tools

--- Sample/Xrm.CI.Framework.Sample/Customizations/Solutions/xRMCISample/Extract.ps1
+++ Sample/Xrm.CI.Framework.Sample/Customizations/Solutions/xRMCISample/Extract.ps1
@@ -2,15 +2,11 @@
 
 param
 (
-	[string]$CrmConnectionString #The connection string as per CRM Sdk
+    [string]$CrmConnectionString, #The connection string as per CRM Sdk
+	[string]$Key #The key for the stored connection string
 )
 
 $ErrorActionPreference = "Stop"
-
-if ($CrmConnectionString -eq '')
-{
-	$CrmConnectionString = $Env:CrmConnection
-}
 
 #Script Location
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
@@ -18,4 +14,27 @@ Write-Verbose "Script Path: $scriptPath"
 
 Write-Verbose "ConnectionString = $connectionString"
 
-& "$scriptPath\..\..\..\packages\XrmCIFramework.9.0.0.31\tools\ExtractSolution.ps1" -Verbose -CoreToolsPath "$scriptPath\..\..\..\packages\Microsoft.CrmSdk.CoreTools.9.0.2.6\content\bin\coretools" -UnpackedFilesFolder "$scriptPath\Customizations" -mappingFile "$scriptPath\mapping.xml" -solutionName "xRMCISample" -solutionFile $null -connectionString "$CrmConnectionString" -TreatPackWarningsAsErrors $false -PackageType "both"
+$matches = Get-ChildItem -Path "$scriptPath\..\..\..\packages\XrmCIFramework.*" | Sort-Object Name -Descending
+if ($matches.Length -gt 0)
+{
+	$frameworkPath = $matches[0].FullName
+	Write-Verbose "Using XrmCIFramework: $frameworkPath"
+}
+else
+{
+	throw "XrmCIFramework not found in nuget packages"
+}
+
+Import-Module "$frameworkPath\tools\Xrm.Framework.CI.PowerShell.Cmdlets.psd1"
+
+if ($CrmConnectionString)
+{
+	Write-Verbose "Using supplied connection string"
+}
+else
+{
+	Write-Verbose "Using connection store"
+	$CrmConnectionString = GetXrmConnectionFromConfig($key);
+}
+
+& "$frameworkPath\tools\ExtractSolution.ps1" -Verbose -CoreToolsPath "$scriptPath\..\..\..\packages\Microsoft.CrmSdk.CoreTools.9.0.2.12\content\bin\coretools" -UnpackedFilesFolder "$scriptPath\Customizations" -mappingFile "$scriptPath\mapping.xml" -solutionName "xRMCISample" -solutionFile $null -connectionString "$CrmConnectionString" -TreatPackWarningsAsErrors $false -PackageType "both"
--- Sample/Xrm.CI.Framework.Sample/Customizations/Solutions/xRMCISample/Pack.ps1
+++ Sample/Xrm.CI.Framework.Sample/Customizations/Solutions/xRMCISample/Pack.ps1
@@ -12,4 +12,15 @@ Write-Verbose "Script Path: $scriptPath"
 
 Write-Verbose "ConnectionString = $connectionString"
 
-& "$scriptPath\..\..\..\packages\XrmCIFramework.9.0.0.31\tools\PackSolution.ps1" -Verbose -CoreToolsPath "$scriptPath\..\..\..\packages\Microsoft.CrmSdk.CoreTools.9.0.2.6\content\bin\coretools" -unpackedFilesFolder "$scriptPath\Customizations" -mappingFile "$scriptPath\mapping.xml" -PackageType Both -TreatPackWarningsAsErrors $false -UpdateVersion $false -OutputPath "C:\temp"
+$matches = Get-ChildItem -Path "$scriptPath\..\..\..\packages\XrmCIFramework.*" | Sort-Object Name -Descending
+if ($matches.Length -gt 0)
+{
+	$frameworkPath = $matches[0].FullName
+	Write-Verbose "Using XrmCIFramework: $frameworkPath"
+}
+else
+{
+	throw "XrmCIFramework not found in nuget packages"
+}
+
+& "$frameworkPath\tools\PackSolution.ps1" -Verbose -CoreToolsPath "$scriptPath\..\..\..\packages\Microsoft.CrmSdk.CoreTools.9.0.2.6\content\bin\coretools" -unpackedFilesFolder "$scriptPath\Customizations" -mappingFile "$scriptPath\mapping.xml" -PackageType Both -TreatPackWarningsAsErrors $false -UpdateVersion $false -OutputPath "C:\temp"
